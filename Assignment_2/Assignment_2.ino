02fc7e8bad36f60d1a517f4baa6d902dca809943
medansari <m3dansari@hotmail.com>
2022-03-04 16:35:14 +0000

    Adding task 4 and task 5

--- Assignment_2/Assignment_2.ino
+++ Assignment_2/Assignment_2.ino
@@ -4,7 +4,13 @@
 #define Push_button 22
 #define SigB 21
 #define SigG 15
-#define Sensor 14
+#define POT 14
+
+const int numPOT_values = 4;    // size of the array for storing potentiometer values
+int POT_Value[numPOT_values];   // readings from the POT input
+int POT_index;                  // POT value indexing
+int POT_total = 0;              // POT total
+int POT_average = 0;            // POT average
 
 
 
@@ -14,8 +20,13 @@ void setup()
   pinMode(Push_button, INPUT);
   pinMode(SigB, OUTPUT);
   pinMode(SigG, INPUT);
-  pinMode(Sensor, INPUT);
+  pinMode(POT, INPUT);
   Serial.begin(9600);
+
+  // initiallising all potentiometer values to be 0
+  for (int init_POT = 0; init_POT < numPOT_values; init_POT++){
+    POT_Value[init_POT] = 0;
+  }
 
 }
 
@@ -37,3 +48,26 @@ void task2()
   Serial.println(buttonState);
 }
 
+void task3()
+{
+
+}
+
+void task4()
+{
+  POT_total = POT_total - POT_Value[POT_index];
+  POT_Value[POT_index] = analogRead(POT);
+}
+
+void task5()
+{
+  POT_total = POT_total + POT_Value[POT_index];
+  POT_index = POT_index + 1;
+
+  if (POT_index >= numPOT_values)
+  {
+    POT_index = 0;
+  } 
+  POT_average = POT_total/numPOT_values;
+  Serial.println(POT_average);
+}
